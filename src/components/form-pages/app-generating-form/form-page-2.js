9252f3e8aba69102320ea497a69ec156b9365265
nikolaevv <starlei165@gmail.com>
2021-09-21 17:58:10 +0300

    feat: Add models board to 2nd form page

--- src/components/form-pages/app-generating-form/form-page-2.js
+++ src/components/form-pages/app-generating-form/form-page-2.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Button, Typography } from '@material-ui/core';
+import { Button, Typography, IconButton } from '@material-ui/core';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 const FormPageTwo = ({formik}) => {
     return (
         <div>
             <Typography color="seconary" variant="h5">2 of 2</Typography>
-        
+
+            <div className="models-header">
+                <Typography color="seconary" variant="h4">Models</Typography>
+
+                <IconButton>
+                    <AddBoxIcon color="primary"/>
+                </IconButton>
+            </div>
+            
+
             <Button
                 variant="contained"
                 type="submit"
