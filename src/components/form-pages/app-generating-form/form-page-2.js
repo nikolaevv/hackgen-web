801121c31b33b24799273c6b8c5442be097c9e57
nikolaevv <starlei165@gmail.com>
2021-10-09 21:52:36 +0300

    feat: Add support of relations

--- src/components/form-pages/app-generating-form/form-page-2.js
+++ src/components/form-pages/app-generating-form/form-page-2.js
@@ -49,90 +49,119 @@ const FormPageTwo = ({formik}) => {
             <div>
                 <div>
                     {values.models && values.models.length > 0 ? (
-                        values.models.map((model, idx) => (
-                            <div className="model-container" key={idx}>
-                                <div>
-                                    <TextField
-                                        value={model.title}
-                                        id={`models.${idx}.title`}
-                                        name={`models.${idx}.title`}
-                                        onChange={formik.handleChange}
-                                        variant="standard"
-                                        className="model-title"
-                                    />
+                        values.models.map((model, idx) => {
+                            const otherModels = [...values.models.slice(0, idx), ...values.models.slice(idx + 1)];
 
-                                    <IconButton onClick={() => onModelDeleted(values.models, idx)} color="primary" aria-label="delete model" component="span">
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </div>
+                            return (
+                                <div className="model-container" key={idx}>
                                     <div>
-                                        {values.models[idx].fields && values.models[idx].fields.length > 0 ? (
-                                            values.models[idx].fields.map((field, fieldIdx) => (
-                                                <div className="model-field" key={fieldIdx}>
-                                                    <TextField
-                                                        fullWidth
-                                                        label="Name"
-                                                        variant="outlined"
-                                                        value={field.name}
-                                                        onChange={formik.handleChange}
-                                                        id={`models.${idx}.fields.${fieldIdx}.name`}
-                                                        name={`models.${idx}.fields.${fieldIdx}.name`}
-                                                    />
+                                        <TextField
+                                            value={model.title}
+                                            id={`models.${idx}.title`}
+                                            name={`models.${idx}.title`}
+                                            onChange={formik.handleChange}
+                                            variant="standard"
+                                            className="model-title"
+                                        />
 
-                                                    <FormControl fullWidth>
-                                                        <InputLabel id={`type-selector-${idx}-${fieldIdx}`}>Type</InputLabel>
-                                                        <Select
-                                                            labelid={`type-selector-${idx}-${fieldIdx}`}
-                                                            id={`models.${idx}.fields.${fieldIdx}.type`}
-                                                            name={`models.${idx}.fields.${fieldIdx}.type`}
-                                                            value={field.type}
-                                                            onChange={formik.handleChange}
-                                                            label="Type"
-                                                        >
-                                                            <MenuItem value={"Integer"}>Integer</MenuItem>
-                                                            <MenuItem value={"String"}>String</MenuItem>
-                                                            <MenuItem value={"Float"}>Float</MenuItem>
-                                                            <MenuItem value={"Boolean"}>Boolean</MenuItem>
-                                                            <MenuItem value={"Relation"}>Relation</MenuItem>
-                                                        </Select>
-                                                    </FormControl>
-
-                                                    <TextField
-                                                        fullWidth
-                                                        label="Default value"
-                                                        variant="outlined"
-                                                        value={field.default}
-                                                        onChange={formik.handleChange}
-                                                        id={`models.${idx}.fields.${fieldIdx}.default`}
-                                                        name={`models.${idx}.fields.${fieldIdx}.default`}
-                                                    />
-
-                                                    <FormControlLabel control={
-                                                        <Checkbox
-                                                            checked={field.nullable}
-                                                            onChange={formik.handleChange}
-                                                            id={`models.${idx}.fields.${fieldIdx}.nullable`}
-                                                            name={`models.${idx}.fields.${fieldIdx}.nullable`}
-                                                        />
-                                                    } label="null acceptable" />
-                                                    
-                                                    <IconButton onClick={() => onModelFieldDeleted(model, idx, fieldIdx)} color="primary" aria-label="delete model field" component="span">
-                                                        <DeleteIcon />
-                                                    </IconButton>
-                                                </div>
-                                            ))
-                                        ) : <div></div>}
+                                        <IconButton onClick={() => onModelDeleted(values.models, idx)} color="primary" aria-label="delete model" component="span">
+                                            <DeleteIcon />
+                                        </IconButton>
                                     </div>
-                                    <Button 
-                                        className="model-field-add-btn" 
-                                        size="small" 
-                                        variant="outlined" 
-                                        onClick={() => onModelFieldAdded(model, idx)}
-                                    >
-                                        Add field
-                                    </Button>
-                            </div>
-                        ))
+                                        <div>
+                                            {values.models[idx].fields && values.models[idx].fields.length > 0 ? (
+                                                values.models[idx].fields.map((field, fieldIdx) => (
+                                                    <div className="model-field" key={fieldIdx}>
+                                                        {
+                                                            field.type === 'Relation' ? (
+                                                                <FormControl fullWidth>
+                                                                    <InputLabel id={`relation-selector-${idx}-${fieldIdx}`}>Relation to</InputLabel>
+                                                                    <Select
+                                                                        labelid={`relation-selector-${idx}-${fieldIdx}`}
+                                                                        id={`models.${idx}.fields.${fieldIdx}.name`}
+                                                                        name={`models.${idx}.fields.${fieldIdx}.name`}
+                                                                        value={field.name}
+                                                                        onChange={formik.handleChange}
+                                                                        label="Relation to"
+                                                                    >
+                                                                        {
+                                                                            otherModels.map((realtionModel, relModelIdx) => (
+                                                                                <MenuItem key={relModelIdx} value={realtionModel.title.toLowerCase()}>{realtionModel.title.toLowerCase()}</MenuItem>
+                                                                            ))
+                                                                        }
+                                                                    </Select>
+                                                                </FormControl>
+                                                            ) : (
+                                                                <TextField
+                                                                fullWidth
+                                                                label="Name"
+                                                                variant="outlined"
+                                                                value={field.name}
+                                                                onChange={formik.handleChange}
+                                                                id={`models.${idx}.fields.${fieldIdx}.name`}
+                                                                name={`models.${idx}.fields.${fieldIdx}.name`}
+                                                            />
+                                                            )
+                                                        }
+
+                                                        <FormControl fullWidth>
+                                                            <InputLabel id={`type-selector-${idx}-${fieldIdx}`}>Type</InputLabel>
+                                                            <Select
+                                                                labelid={`type-selector-${idx}-${fieldIdx}`}
+                                                                id={`models.${idx}.fields.${fieldIdx}.type`}
+                                                                name={`models.${idx}.fields.${fieldIdx}.type`}
+                                                                value={field.type}
+                                                                onChange={formik.handleChange}
+                                                                label="Type"
+                                                            >
+                                                                <MenuItem value={"Integer"}>Integer</MenuItem>
+                                                                <MenuItem value={"Text"}>String</MenuItem>
+                                                                <MenuItem value={"Date"}>Date</MenuItem>
+                                                                <MenuItem value={"DateTime"}>DateTime</MenuItem>
+                                                                <MenuItem value={"enum"}>Text Choice</MenuItem>
+                                                                <MenuItem value={"Float"}>Float</MenuItem>
+                                                                <MenuItem value={"Boolean"}>Boolean</MenuItem>
+                                                                <MenuItem value={"Relation"}>Relation</MenuItem>
+                                                            </Select>
+                                                        </FormControl>
+
+                                                        <TextField
+                                                            fullWidth
+                                                            label="Default value"
+                                                            variant="outlined"
+                                                            value={field.default}
+                                                            onChange={formik.handleChange}
+                                                            id={`models.${idx}.fields.${fieldIdx}.default`}
+                                                            name={`models.${idx}.fields.${fieldIdx}.default`}
+                                                        />
+
+                                                        <FormControlLabel control={
+                                                            <Checkbox
+                                                                checked={field.nullable}
+                                                                onChange={formik.handleChange}
+                                                                id={`models.${idx}.fields.${fieldIdx}.nullable`}
+                                                                name={`models.${idx}.fields.${fieldIdx}.nullable`}
+                                                            />
+                                                        } label="null acceptable" />
+                                                        
+                                                        <IconButton onClick={() => onModelFieldDeleted(model, idx, fieldIdx)} color="primary" aria-label="delete model field" component="span">
+                                                            <DeleteIcon />
+                                                        </IconButton>
+                                                    </div>
+                                                ))
+                                            ) : <div></div>}
+                                        </div>
+                                        <Button 
+                                            className="model-field-add-btn" 
+                                            size="small" 
+                                            variant="outlined" 
+                                            onClick={() => onModelFieldAdded(model, idx)}
+                                        >
+                                            Add field
+                                        </Button>
+                                </div>
+                            )
+                        })
                     ) : <div/>}
                 </div>
             </div>
