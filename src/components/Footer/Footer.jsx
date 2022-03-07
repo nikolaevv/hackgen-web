14919179e797126a0d1194f8ce16f564368524fc
nikolaevv <starlei165@gmail.com>
2022-03-07 16:20:10 +0300

    feat: Github button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Container, Link, Typography } from '@material-ui/core';
 import './Footer.styles.css';
 
+import githubButton from '../../assets/github.png'
+
 const Footer = (props) => (
   <div className="FooterWrapper">
     <Container>
+      <Link href="https://github.com/nikolaevv/hackgen">
+        <img className="github-btn" src={githubButton} alt="github"/>
+      </Link>
       <Typography variant="body1">Support: <Link href="mailto:starlei165@gmail.com">starlei165@gmail.com</Link> </Typography>
     </Container>
   </div>
